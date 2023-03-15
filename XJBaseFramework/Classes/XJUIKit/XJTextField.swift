42c00aa802a81e825f7072a02d6494aba6cd8fe6
AndreaArlex <546113971@qq.com>
2023-03-15 17:19:08 +0800

    feat：增加UITextField的基类

---
+++ XJBaseFramework/Classes/XJUIKit/XJTextField.swift
@@ -0,0 +1,20 @@
+//
+//  XJTextField.swift
+//  XJBaseFramework
+//
+//  Created by AndreaArlex on 2023/3/15.
+//
+
+import UIKit
+
+open class XJTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
